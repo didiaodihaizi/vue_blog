23e12a0dd78f4238a2e391a4bd11f6206e5f183b
xuzhihao <xuzhihao@cmcm.com>
2019-10-20 03:21:32 +0800

    chore:修改构建错误问题

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue_blog',
+    assetsPublicPath: '/vue_blog/',
 
     /**
      * Source Maps
