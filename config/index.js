8a5f8920cf34adb08ee4f850ae544ce9506dff43
xuzhihao <xuzhihao@cmcm.com>
2019-10-21 20:50:45 +0800

    chore:修改构建的assetspublckpath为my-blog

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue_blog/',
+    assetsPublicPath: '/my-blog/',
 
     /**
      * Source Maps
